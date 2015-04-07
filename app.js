4864d4818caa1db26295746cf963b34f23ca835c
izestrea <izestrea@gmail.com>
2015-04-07 16:35:29 +0300

    Using ng-repeat

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ var app = angular.module('app',[]);
 // }
 
 app.controller('MainController', function($scope){
+	$scope.ob = {name: "mike", age: 34, title: "Mr."}
+	// $scope.array = [1, 2, 3, 4, 5, 5, 7, 8, 9]
+	// $scope.users = [{name: "john", age: 32}, {name: "andy", age: 28}, {name: "sam", age: 23}, {name: "sam", age: 23}]
 	// $scope.val = "test456"
-	$scope.val  = 1
-	$scope.even = false
+	// $scope.val  = 1
+	// $scope.even = false
 
-	$scope.inc = function(){
-		$scope.val += 1
-		$scope.even = $scope.val % 2 == 0
-	}
+	// $scope.inc = function(){
+	// 	$scope.val += 1
+	// 	$scope.even = $scope.val % 2 == 0
+	// }
 	// $scope.isEven = function(){
 		// return $scope.val % 2 == 0
 	// }
