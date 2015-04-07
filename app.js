a0c69bc06a1e15762175a82189c4b3c1a5292fc1
izestrea <izestrea@gmail.com>
2015-04-07 14:52:53 +0300

    Methods to declare controllers

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-angular.module('app',[]);
+var app = angular.module('app',['']);
+// var app = angular.module('app',['controllers']);
+
+// var MainController = function($scope) {
+// 	$scope.val = "test123"
+// 	$scope.func = function(){
+// 		return "abc" + "def"
+// 	}
+// }
+
+app.controller('MainController', function($scope){
+	$scope.val = "test456"
+})
+
+// angular.module('controllers', []).controller('MainController', function($scope){
+// 	$scope.val = "test789"
+// })
