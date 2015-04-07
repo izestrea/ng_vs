0656e962da0ea2c1a6f81b0ea0b7f3d10772bc10
izestrea <izestrea@gmail.com>
2015-04-07 15:06:42 +0300

    Built-in directives

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app',['']);
+var app = angular.module('app',[]);
 // var app = angular.module('app',['controllers']);
 
 // var MainController = function($scope) {
@@ -9,7 +9,17 @@ var app = angular.module('app',['']);
 // }
 
 app.controller('MainController', function($scope){
-	$scope.val = "test456"
+	// $scope.val = "test456"
+	$scope.val  = 1
+	$scope.even = false
+
+	$scope.inc = function(){
+		$scope.val += 1
+		$scope.even = $scope.val % 2 == 0
+	}
+	// $scope.isEven = function(){
+		// return $scope.val % 2 == 0
+	// }
 })
 
 // angular.module('controllers', []).controller('MainController', function($scope){
